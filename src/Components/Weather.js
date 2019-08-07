8691b875863f5d51adbaa76d857fb366196fb730
adam mckenzie <adammckenzie@adams-MacBook-Pro-3.local>
2019-08-07 16:39:57 -0500

    Math floored fahrenheit

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -3,7 +3,7 @@ const Weather = (props) => {
   return(
       <div>
       {props.country && props.city && <p>Location: {props.city}, {props.country}</p>}
-      {props.temperature && <p>Temperature: {props.temperature}</p>}
+      {props.temperature && <p>Temperature: {Math.floor(props.temperature)}</p>}
       {props.humidity && <p>Humidity: {props.humidity}</p>}
       {props.description && <p>Conditions:  {props.description}</p>}
       {props.error && <p>{props.error}</p>}
