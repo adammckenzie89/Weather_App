6bdc57c3efc57c2049cf2cc7449cd281141b1b2a
adam mckenzie <mckenziesadam@gmail.com>
2019-09-20 19:14:16 -0500

    weather icons

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React from "react";
+// import styles from "./Home.module.scss";
 
-const Weather = (props) => {
-  return(
-      <div>
-      {props.country && props.city && <p>Location: {props.city}, {props.country}</p>}
+const Weather = props => {
+  return (
+    <div>
+      {props.country && props.city && (
+        <p>
+          Location: {props.city}, {props.country}
+        </p>
+      )}
       {props.temperature && <p>Temperature: {Math.floor(props.temperature)}</p>}
       {props.humidity && <p>Humidity: {props.humidity}%</p>}
-      {props.description && <p>Conditions:  {props.description}</p>}
+      {props.description &&
+        (<p>Conditions: {props.description}</p> && (
+          <img
+            // className={styles.weather}
+            src="http://openweathermap.org/img/wn/10d@2x.png"
+          />
+        ))}
       {props.error && <p>{props.error}</p>}
     </div>
-   )
-}
+  );
+};
 
 export default Weather;
--- src/Home.js
+++ src/Home.js
@@ -1,75 +1,75 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
-import css from "./App.css";
 import Form from "./Components/Form";
 import Titles from "./Components/Titles";
 import Weather from "./Components/Weather";
 import styles from "./Home.module.scss";
 
+const REACT_APP_KEY = "fd8a7245b8a05140321698de9c5d573f";
 
-const REACT_APP_KEY ='fd8a7245b8a05140321698de9c5d573f';
-
-class Home extends Component{
+class Home extends Component {
   state = {
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: ""
-    }
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: ""
+  };
 
-  getWeather = async (e) => {
-  e.preventDefault();
-  const city = e.target.elements.city.value;
-  const country = e.target.elements.country.value;
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
 
-  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${REACT_APP_KEY}`).then(response => {
-    if(city && country){
-    this.setState({
-      temperature: response.data.main.temp,
-      city: response.data.name,
-      country: response.data.sys.country,
-      humidity: response.data.main.humidity,
-      description: response.data.weather[0].description,
-      error: ""
-    })
-    }else{
-      this.setState({
-        error: "Please enter a location"
-      })
-    }
-    console.log(response)
-  })
-  }
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${REACT_APP_KEY}`
+      )
+      .then(response => {
+        if (city && country) {
+          this.setState({
+            temperature: response.data.main.temp,
+            city: response.data.name,
+            country: response.data.sys.country,
+            humidity: response.data.main.humidity,
+            description: response.data.weather[0].description,
+            error: ""
+          });
+        } else {
+          this.setState({
+            error: "Please enter a location"
+          });
+        }
+        console.log(response);
+      });
+  };
 
-  render(){
-  const time = new Date().toLocaleString()
-    return(
-      <main >
+  render() {
+    const time = new Date().toLocaleString();
+    return (
+      <main>
         <section className={styles.letSee}>
           <div className={styles.titles}>
-        <Titles />
+            <Titles />
           </div>
         </section>
         <section className={styles.form}>
-        <Form 
-        loadWeather={this.getWeather} />
+          <Form loadWeather={this.getWeather} />
         </section>
         <section className={styles.weather}>
-        <Weather 
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error} />
+          <Weather
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.humidity}
+            temperature={this.state.temperature}
+            description={this.state.description}
+            error={this.state.error}
+          />
         </section>
-        <div className={styles.time}>
-        {time}
-        </div>
+        <div className={styles.time}>{time}</div>
       </main>
-   )
+    );
   }
 }
 
